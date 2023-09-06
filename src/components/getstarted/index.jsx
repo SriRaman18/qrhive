59ac94df166130cb2ebdcfaacbca6459303126db
SriRaman18 <sriraman434@gmail.com>
2023-09-06 19:45:47 +0100

    second commit

--- src/components/getstarted/index.jsx
+++ src/components/getstarted/index.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Getstarted() {
-  return (
-    <div>
-      Getstarted
-    </div>
-  );
+  return <div>Getstarted now</div>;
 }
 
 export default Getstarted;
