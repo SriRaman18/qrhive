9af36e4b45e4fbfd2a692de412f46a4ed1f66b5c
SriRaman18 <sriraman434@gmail.com>
2023-09-25 19:34:19 +0100

    media query updated

--- src/components/basic/navbar/index.jsx
+++ src/components/basic/navbar/index.jsx
@@ -12,12 +12,12 @@ function Navbar({ highlightButton }) {
   const navigate = useNavigate();
 
   const [showSideNavbar, setShowSideNavbar] = useState(false);
-  const navbar = document.getElementById("navbar");
-  document.body.addEventListener("click", (event) => {
-    if (navbar && !navbar.contains(event.target)) {
-      setShowSideNavbar(false);
-    }
-  });
+  // const navbar = document.getElementById("navbar");
+  // document.body.addEventListener("click", (event) => {
+  //   if (navbar && !navbar.contains(event.target)) {
+  //     setShowSideNavbar(false);
+  //   }
+  // });
   return (
     <div className="nav" id="navbar">
       <div className="logo">
